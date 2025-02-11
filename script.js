007a41cfb5fc06ac785eb4151b3dacd4f4b3062f
Dayosalako <126185613+Dayosalako@users.noreply.github.com>
2025-02-11 12:33:19 +0100

    Update script.js

--- script.js
+++ script.js
@@ -90,31 +90,7 @@ document.addEventListener("DOMContentLoaded", () => {
     };
 
 // news and events
-    let currentIndex = 0;
-
-function showSlide(index) {
-    const slides = document.querySelectorAll(".event-slide");
-    const slider = document.querySelector(".slider");
-
-    if (index >= slides.length) {
-        currentIndex = 0;
-    } else if (index < 0) {
-        currentIndex = slides.length - 1;
-    } else {
-        currentIndex = index;
-    }
-
-    let newTransformValue = -currentIndex * 100 + "%";
-    slider.style.transform = "translateX(" + newTransformValue + ")";
-}
-
-function nextSlide() {
-    showSlide(currentIndex + 1);
-}
-
-function prevSlide() {
-    showSlide(currentIndex - 1);
-}
+  
 
 // Auto slide every 5 seconds
 setInterval(nextSlide, 5000);
